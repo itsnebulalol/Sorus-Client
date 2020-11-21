ebfaa1b05718f456b38adcfa8e93cda0de2ac7c7
Danterus <>
2020-11-20 20:44:23 -0800

    fixed some small bugs regarding nametag direction

--- Versions/1.8.9/src/main/java/org/sorus/oneeightnine/injectors/perspective/RenderHook.java
+++ Versions/1.8.9/src/main/java/org/sorus/oneeightnine/injectors/perspective/RenderHook.java
@@ -8,12 +8,24 @@ public class RenderHook {
 
     public static float getRotationPitch() {
         Perspective perspective = Sorus.getSorus().getModuleManager().getModule(Perspective.class);
-        return perspective.isToggled() ? perspective.getRotationPitch() : Minecraft.getMinecraft().thePlayer.rotationPitch;
+        float renderPitch = Minecraft.getMinecraft().thePlayer.rotationPitch;
+        if(Minecraft.getMinecraft().gameSettings.thirdPersonView == 2) {
+            renderPitch = -renderPitch;
+        }
+        return perspective.isToggled() ? perspective.getRotationPitch() : renderPitch;
     }
 
     public static float getRotationYaw() {
         Perspective perspective = Sorus.getSorus().getModuleManager().getModule(Perspective.class);
-        return perspective.isToggled() ? perspective.getRotationYaw() : Minecraft.getMinecraft().thePlayer.rotationYaw;
+        float renderYaw = Minecraft.getMinecraft().thePlayer.rotationYaw;
+        if(Minecraft.getMinecraft().gameSettings.thirdPersonView == 2) {
+            if(renderYaw > 180) {
+                renderYaw -= 180;
+            } else {
+                renderYaw += 180;
+            }
+        }
+        return perspective.isToggled() ? perspective.getRotationYaw() : renderYaw;
     }
 
 }
--- Versions/1.8.9/src/main/java/org/sorus/oneeightnine/injectors/perspective/RenderInjector.java
+++ Versions/1.8.9/src/main/java/org/sorus/oneeightnine/injectors/perspective/RenderInjector.java
@@ -6,6 +6,7 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.sorus.client.obfuscation.ObfuscationManager;
 import org.sorus.client.startup.injection.Hook;
 import org.sorus.client.startup.injection.Injector;
 import org.sorus.client.startup.injection.Modify;
@@ -19,9 +20,11 @@ public class RenderInjector extends Injector<Render<?>> {
 
     @Modify(name = "renderLivingLabel", desc = "(Lnet/minecraft/entity/Entity;Ljava/lang/String;DDDI)V")
     public static void modifyRenderLivingLabel(MethodNode methodNode) {
+        String renderManagerClass = ObfuscationManager.getClassName("net/minecraft/client/renderer/entity/Render");
+        String renderManagerField = ObfuscationManager.getFieldName("net/minecraft/client/renderer/entity/Render", "renderManager");
         int i = 0;
         for(AbstractInsnNode node : methodNode.instructions.toArray()) {
-            if(node instanceof FieldInsnNode && ((FieldInsnNode) node).name.equals("renderManager")) {
+            if(node instanceof FieldInsnNode && ((FieldInsnNode) node).owner.equals (renderManagerClass) && ((FieldInsnNode) node).name.equals(renderManagerField)) {
                 switch(i) {
                     case 1: {
                         AbstractInsnNode node1 = node.getPrevious().getPrevious();
